5c720da817e4788822fff5a737024c127a872d7a
Cookiewookie87 <jernej.tratnik@gmail.com>
2018-07-08 19:01:22 +0200

    Init slideshow
    Without auto pic change. Added pictures

--- js/script.js
+++ js/script.js
@@ -1,12 +1,120 @@
+// navigation selection
 const navigation = document.querySelector("ul.navigation");
 const navigationToggleButton = document.querySelector(".navigation-toggle");
 const navigationList = document.querySelectorAll(".navigation a");
+// background image selector
+const backgroundImgDiv = document.querySelector(".bg");
+const previousImgBtn = document.querySelector(".prev");
+const nextImgBtn = document.querySelector(".next");
+const imgDotBtn = Array.from(document.querySelectorAll(".dot"));
+const arrImg = ['url("../img/0.jpg")', 'url("../img/1.jpg")', 'url("../img/2.jpg")', 'url("../img/3.jpg")'];
+const dot0 = document.querySelector(".dot-0");
+const dot1 = document.querySelector(".dot-1");
+const dot2 = document.querySelector(".dot-2");
+const dot3 = document.querySelector(".dot-3");
+let startImgIndex = 0;
+let currentIndex = 0;
 
+// navigation functions
 function toggleNav() {
     navigation.classList.toggle("active");
 }
 function navLink() {
     navigation.classList.remove("active");
 }
+// background image functions
+function nextImgShow() {
+    startImgIndex++;
+    if (startImgIndex === imgDotBtn.length) {
+        startImgIndex = 0;
+    }
+    currentIndex = startImgIndex;
+    backgroundImgDiv.style.backgroundImage = arrImg[startImgIndex];
+    if (startImgIndex + 1 >= arrImg.length) {
+        startImgIndex = -1;
+    }
+    toggleDotActive(currentIndex);
+}
+function previousImgShow() {
+    startImgIndex--;
+    if (startImgIndex === -1) {
+        startImgIndex = (arrImg.length - 1);
+    }
+    currentIndex = startImgIndex;
+    backgroundImgDiv.style.backgroundImage = arrImg[startImgIndex];
+    if (startImgIndex <= 0) {
+        startImgIndex = arrImg.length;
+    }
+    toggleDotActive(currentIndex);
+}
+
+function dotBtnNavigate() { 
+    if (this.classList.contains("dot-0")) {
+        backgroundImgDiv.style.backgroundImage = arrImg[0];
+        startImgIndex = 0;
+        currentIndex = 0;
+        toggleDotActive(currentIndex);
+    } else if (this.classList.contains("dot-1")) {
+        backgroundImgDiv.style.backgroundImage = arrImg[1];
+        startImgIndex = 1;
+        currentIndex = 1;
+        toggleDotActive(currentIndex);
+    } else if (this.classList.contains("dot-2")) {
+        backgroundImgDiv.style.backgroundImage = arrImg[2];
+        startImgIndex = 2;
+        currentIndex = 2;
+        toggleDotActive(currentIndex);
+    } else if (this.classList.contains("dot-3")) {
+        backgroundImgDiv.style.backgroundImage = arrImg[3];
+        startImgIndex = 3;
+        currentIndex = 3;
+        toggleDotActive(currentIndex);
+    }
+}
+
+function toggleDotActive(currentIndex) {
+    switch(currentIndex) {
+        case 0:
+            dot0.classList.add("dot-active");
+            dot1.classList.remove("dot-active");
+            dot2.classList.remove("dot-active");
+            dot3.classList.remove("dot-active");
+            break;
+        case 1:
+            dot0.classList.remove("dot-active");
+            dot1.classList.add("dot-active");
+            dot2.classList.remove("dot-active");
+            dot3.classList.remove("dot-active");
+            break;
+        case 2:
+            dot0.classList.remove("dot-active");
+            dot1.classList.remove("dot-active");
+            dot2.classList.add("dot-active");
+            dot3.classList.remove("dot-active");
+            break;
+        case 3:
+            dot0.classList.remove("dot-active");
+            dot1.classList.remove("dot-active");
+            dot2.classList.remove("dot-active");
+            dot3.classList.add("dot-active");
+            break;
+        default:
+            break;
+    }
+}
+
+// navigation events
 navigationToggleButton.addEventListener("click", toggleNav);
 navigationList.forEach(item => item.addEventListener("click", navLink));
+
+// background image event
+nextImgBtn.addEventListener("click", nextImgShow);
+nextImgBtn.addEventListener("click", resetSlideshowTimer);
+
+previousImgBtn.addEventListener("click", previousImgShow);
+imgDotBtn.forEach(btn => btn.addEventListener("click", dotBtnNavigate));
+
+
+
+
+
